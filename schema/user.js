43402a08fb94a1357719156ef13b6aa541af0c0d
akif <su92-bssem-f22-118@superior.edu.pk>
2025-04-12 00:14:31 +0500

    server_side

---
+++ schema/user.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+const SignupSchema = new Schema({
+  username: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  phone: { type: Number },
+  date_of_birth: { type: Date },
+  password: { type: String, required: true },
+  country: { type: String },
+  image: { type: String },
+  Userbio: { type: String },
+  role: {type: String, enum: ["machanic", "user"], default: "user"},
+});
+export default mongoose.model("Accounts", SignupSchema);
