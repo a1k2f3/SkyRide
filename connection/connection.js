694c919c6a8ccf7ab8b0d32988de55a70a7fc785
akif <su92-bssem-f22-118@superior.edu.pk>
2025-04-13 00:05:10 +0500

    edit connection .js and add dotenv

--- connection/connection.js
+++ connection/connection.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 // const dburl=process.env.DATA_BASE_URL
 const Connection=async()=>{
 try {
-    await mongoose.connect('mongodb+srv://akifbutt935:akif1234@cluster0.awqe4fp.mongodb.net/');
+    await mongoose.connect('mongodb+srv://akifbutt935:akif1234@cluster0.awqe4fp.mongodb.net/skyrides');
   console.log("databsae cnnection succesfull")
 } catch (error) {
     console.log(error);
