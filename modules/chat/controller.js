160bbd07d2ea50edf364499c3fdb9c6b12c6fbe3
akif <su92-bssem-f22-118@superior.edu.pk>
2025-04-23 01:06:07 +0500

    hello

--- modules/chat/controller.js
+++ modules/chat/controller.js
@@ -7,22 +7,19 @@ export const sendMessage = async (req, res) => {
     const newMessage = new Chats({ senderId, receiverId, message, role });
     await newMessage.save();
     req.io.to(`room-${senderId}-${receiverId}`).emit('receive-message', { senderId, message });
-
     res.status(201).json(newMessage);
   } catch (error) {
     console.error("❌ Error sending message:", error.message);
     res.status(500).json({ error: 'Failed to send message' });
   }
 };
-
-// Get chat history between user and mechanic
 export const getChatHistory = async (req, res) => {
-  const { userId, mechanicId } = req.query;
+  const { userId, userId2 } = req.query;
   try {
     const chatHistory = await Chats.find({
       $or: [
-        { senderId: userId, receiverId: userId },
-        { senderId: userId, receiverId: userId }
+        { senderId: userId, receiverId: userId2 },
+        { senderId: userId2, receiverId: userId }
       ]
     }).sort({ timestamp: 1 });
     res.status(200).json(chatHistory);
