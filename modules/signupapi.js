f73c177a7c26653ead1c957dbe56d0d36f142f01
akif <su92-bssem-f22-118@superior.edu.pk>
2025-04-25 03:03:25 +0500

    ih

--- modules/signupapi.js
+++ modules/signupapi.js
@@ -77,10 +77,9 @@ export default (io) => {
         port: 465,
         auth: {
           user: process.env.EMAIL_USER,
-          pass: process.env,EMAIL_PASS,
+          pass: process.env.EMAIL_PASS,
         }
       });
-
       const mailOptions = {
         from: process.env.EMAIL_USER,
         to: email,
